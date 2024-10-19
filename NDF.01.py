0154bd3095d33af3602776564637a882d1c629a8
Guilherme <guilherme.figura@icloud.com>
2024-10-19 12:12:13 -0300

    Pergunta

---
+++ NDF.01.py
@@ -0,0 +1,10 @@
+"""Um pecuarista possui 100 cabeças de gado magro no pasto, e estima-se que deverá levar seis meses para engordar e vender os bois para o
+frigorífico pesando, em média, 25 arrobas cada um deles. Hoje, o valor da arroba de boi à vista está em R$ 240.
+Preocupado com o preço da carne no mercado interno, o criador poderia fazer a venda a termo de 2.500 arrobas de boi a um preço de R$ 249 por arroba
+ao frigorífico, para o prazo de seis meses. Dessa maneira, o criador poderia garantir o seu lucro e preocupar-se apenas com os problemas relacionados
+à criação do gado. Ao final de seis meses, no vencimento da operação a termo, considere dois cenários: no primeiro, a arroba de boi gordo à vista
+está cotada a R$ 234 e no segundo R$ 255.
+Compare os cenários, qual deverá ser o ganho do pecuarista na operação a termo, isoladamente? Qual a receita e o fluxo de caixa do pecuarista?
+O que a operação a termo lhe proporcionou? """
+
+
